e009fd2974f41fd0d26a93855d72f7186a823c39
JosephHanJL <jh4632@columbia.edu>
2024-04-13 16:48:18 -0400

    version of hazard without IF bug

--- programs/hazard.s
+++ programs/hazard.s
@@ -1,6 +1,6 @@
 data = 0x1000				#PC NPC
-lw x4, 0(x1)				#0 4
-#addi x4, x0, 1
+#lw x4, 0(x1)				#0 4
+addi x4, x0, 1
 #Init Registers
 addi x1, x0, 1				#4 8
 addi x2, x0, 2				#8 12
@@ -13,9 +13,7 @@ addi x2, x0, 2				#8 12
 # inst dest, opA/rs1, opB/rs2
 #Hazards Checking
 lw x1, 0(x4)				#12 16
-#addi x1, x0, 0
-#mul	x3,	x2,	x1		#16 20
-add      x3, x2, x1
+mul	x3,	x2,	x1		#16 20
 sw	x3, 0x100(x4)			#20 24
 addi x4, x4, 4				#24 28
 lw x1, 0(x4)				#28 32
