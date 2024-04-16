cf08a67e24b8db0451f00eb2d8641966be5e4fb5
JosephHanJL <jh4632@columbia.edu>
2024-04-16 17:26:12 -0400

    trivial

--- programs/fib_no_loop.s
+++ programs/fib_no_loop.s
@@ -24,15 +24,16 @@
     li	x6, 0x1010
     li	x10, 2
     li	x2, 1
+    li  x3, 1
     #sw	x2, 0(x4)
     #sw	x2, 0(x5)
 #loop:	lw	x2, 0(x4)
     #lw	x3, 0(x5)
-    #add	x3,	x3,	x2 #
+    add	x3,	x3,	x2 #
     addi	x4,	x4,	0x8 #
     addi	x5,	x5,	0x8 #
     addi	x10,	x10,	0x1 #
-    #slti	x11,	x10,	16 #
+    slti	x11,	x10,	16 #
     #sw	x3, 0(x6)
     addi	x6,	x6,	0x8 #
     #bne	x11,	x0,	loop #
