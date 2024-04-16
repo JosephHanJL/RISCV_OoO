9c129c8e4b02259b0470f3e44f736eca15639f76
Evan Battaglia <eb3504@columbia.edu>
2024-04-15 23:55:59 -0400

    initial evan edits, compiles

--- verilog/rob.sv
+++ verilog/rob.sv
@@ -55,11 +55,11 @@ module rob(
     // ----- FIFO internal ----- //
     ///////////////////////////////
     ROB_ENTRY rob_memory [`ROB_SZ :0]; // ROB_SZ default to 8; An empty unit is used to detect full
-
+    
     ROB_TAG head; // head and tail pointer for FIFO
     ROB_TAG tail;
 
-    ROB_ENTRY data_in; // Data input to fifo.
+    ROB_ENTRY data_in, new_tail; // Data input to fifo.
 
     logic full; // FIFO full flag.
     logic empty; // FIFO empty flag.
@@ -68,15 +68,17 @@ module rob(
     //   ROB Operational logic   //
     ///////////////////////////////
     always_comb begin
+        // default to 0s
+        rob_map_packet = '0;
+        new_tail = '0;
+        // prepare new tail entry
+        new_tail.dp_packet = instructions_buffer_rob_packet;
+        new_tail.rob_tag = tail;
+        if (instructions_buffer_rob_packet.has_dest)
+            new_tail.r = instructions_buffer_rob_packet.dest_reg_idx;
+        // fill in map packet
         rob_map_packet.rob_head = rob_memory[head];
-        if (!empty) begin
-            //rob_map_packet.rob_new_tail.rob_tag = rob_memory[(tail - 1) % `ROB_SZ].rob_tag + 1;
-            rob_map_packet.rob_new_tail.rob_tag = ((tail - 1) === 15) ? rob_memory[`ROB_SZ].rob_tag + 1 : rob_memory[tail - 1].rob_tag + 1;
-            rob_rs_packet.rob_tail.rob_tag = ((tail - 1) === 15) ? rob_memory[`ROB_SZ].rob_tag + 1 : rob_memory[tail - 1].rob_tag + 1;
-        end else begin
-            rob_map_packet.rob_new_tail.rob_tag = (tail + 1) % (`ROB_SZ + 1);
-            rob_rs_packet.rob_tail.rob_tag = (tail + 1) % (`ROB_SZ + 1);
-        end
+        rob_map_packet.rob_new_tail = new_tail;
         rob_map_packet.retire_valid = rob_memory[head].complete && rob_memory[head].dp_packet.valid;
 
         // Sending packets to rs:
@@ -97,52 +99,49 @@ module rob(
     ///////////////////////////////
     always_ff @(posedge clock or posedge reset) begin
         if (reset) begin
-            for (int i = 0; i <= `ROB_SZ; i++) begin // FIFO initialation
+            for (int i = 0; i <= `ROB_SZ; i++) begin
                 rob_memory[i] <= '0;
             end
-
             // initialize FIFO signals
-            head            <= 0;
-            tail            <= 0;
-
+            head            <= 1;
+            tail            <= 1;
         end else if (squash_packet.squash_valid) begin
             // Back in time:
-            while (tail !== squash_packet.rob_tag) begin
+            while (tail != squash_packet.rob_tag) begin
                 rob_memory[tail] <= '0;
-                tail = (tail === 0) ? `ROB_SZ : tail - 1;
+                tail = (tail == 0) ? `ROB_SZ : tail - 1;
             end
-                rob_memory[tail] <= '0;	
-	end else begin
+            rob_memory[tail] <= '0;	
+        end else begin
             // Read Logic
             if (!empty && rob_memory[head].complete) begin
                 rob_rt_packet.data_retired <= rob_memory[head];   // Read data from memory.
                 rob_memory[head]           <= '0;
-                head <= (head + 1) % (`ROB_SZ + 1) ; // Increment read pointer with wrap-around.
+                head <= (head >= `ROB_SZ) ? 1 : (head + 1); // Increment read pointer with wrap-around.
             end
 
             // Write Logic
             if (!full && dp_rob_available) begin // Accept data available signal from dispatch
-                rob_memory[tail].dp_packet <= instructions_buffer_rob_packet; // Write dp part into memory.
-                rob_memory[tail].rob_tag <= tail + 1; // Assign the rob_tag
-                if (instructions_buffer_rob_packet.has_dest)
-                    rob_memory[tail].r <= instructions_buffer_rob_packet.dest_reg_idx; // Assign the destination reg to current ROB#
-                tail <= (tail + 1) % (`ROB_SZ + 1); // Increment write pointer with wrap-around.
+                rob_memory[tail] <= new_tail;
+                tail <= (tail >= `ROB_SZ) ? 1 : (tail + 1); // Increment write pointer with wrap-around.
             end
         end       
 
         // Check CDB, and update the broadcast value in fifo
-        if (cdb_rob_packet.rob_tag !== 0)
+        if (cdb_rob_packet.rob_tag !== 0) begin
             for (int index = 0; index <= `ROB_SZ; index++) begin
-                if (rob_memory[index].rob_tag === cdb_rob_packet.rob_tag) begin // What is this for
+                if (rob_memory[index].rob_tag == cdb_rob_packet.rob_tag) begin
                     rob_memory[index].V <= cdb_rob_packet.v;
                     rob_memory[index].complete <= 1'b1;
                 end
-            end                                    
+            end    
+        end                                
     end
 
-    assign full  = ((tail + 1) % (`ROB_SZ + 1) == head) || (instructions_buffer_rob_packet.fu_sel === STORE && ~empty); // What is empty for?
+    assign full  = (tail == head) || (rob_memory[tail].dp_packet.fu_sel == STORE); // What is empty for?
 
-    assign empty = (head == tail);
+    // empty if head=tail and the entry is empty 
+    assign empty = (head == tail) && rob_memory[head].dp_packet.valid == 0;
     assign rob_dp_available = ~full;
 
 //    always_comb begin
