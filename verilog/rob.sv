17b628ecb720cdf574436be127ca40d7bb854a2d
Evan Battaglia <eb3504@columbia.edu>
2024-04-15 23:59:19 -0400

    removed store stall logic from rob

--- verilog/rob.sv
+++ verilog/rob.sv
@@ -138,7 +138,7 @@ module rob(
         end                                
     end
 
-    assign full  = (tail == head) || (rob_memory[tail].dp_packet.fu_sel == STORE); // What is empty for?
+    assign full  = (tail == head)  && rob_memory[head].dp_packet.valid == 1; // this was moved to rs --> || (rob_memory[tail].dp_packet.fu_sel == STORE); // What is empty for?
 
     // empty if head=tail and the entry is empty 
     assign empty = (head == tail) && rob_memory[head].dp_packet.valid == 0;
