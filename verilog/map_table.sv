c399c3a2cb89473fb13cce5e1a1c44175aa94674
Evan Battaglia <eb3504@columbia.edu>
2024-04-19 12:55:23 -0400

    map table bugs with squashing

--- verilog/map_table.sv
+++ verilog/map_table.sv
@@ -25,6 +25,7 @@ module map_table(
 
     // map table memory
     MAP_PACKET [31:0] m_table;
+    MAP_PACKET [`ROB_SZ:0][31:0] m_table_record;
 
     // update the map table field when RS says the dispatch is valid and the inst has a destination reg
     wire write_field = dispatch_valid && dp_packet.has_dest && dp_packet.dest_reg_idx != `ZERO_REG;
@@ -38,12 +39,15 @@ module map_table(
                 m_table[i] <= '0;
             end
         end else begin
+            // store state for later jumps
+            m_table_record[rob_map_packet.rob_new_tail.rob_tag] <= m_table;
             // set t_plus tag where cdb tag matches m_table entry (data should now be found in ROB)
             for (int i = 0; i < 32; i++) begin
                 // rob_tag = 0 means default.
-                if (cdb_packet.rob_tag !== 0)
+                if (cdb_packet.rob_tag !== 0) begin
                     m_table[i].t_plus <= (m_table[i].rob_tag == cdb_packet.rob_tag && m_table[i].rob_tag != `ZERO_REG) ? 1 : m_table[i].t_plus;
                 end
+            end
             // clear table entry when ROB retires an instruction
             if (rob_map_packet.retire_valid) begin
                 for (int i = 0; i < 32; i++) begin
@@ -58,31 +62,24 @@ module map_table(
             if (write_field) begin
                 m_table[dp_packet.dest_reg_idx].rob_tag <= rob_map_packet.rob_new_tail.rob_tag;
                 m_table[dp_packet.dest_reg_idx].t_plus <= 0;
+                // store state of m_table for later rollback if jump
+                m_table_record[rob_map_packet.rob_new_tail.rob_tag][dp_packet.dest_reg_idx].rob_tag <= rob_map_packet.rob_new_tail.rob_tag;
+                m_table_record[rob_map_packet.rob_new_tail.rob_tag][dp_packet.dest_reg_idx].t_plus <= 0;
             end
             // Squash logic
             if (branch_packet.branch_valid) begin
-                // Back in time:
-                for (int i = 0; i <= `ROB_SZ; i++) begin
-                    if (branch_packet.rob_tag <= rob_map_packet.tail) begin
-                        if (i > branch_packet.rob_tag && i <= rob_map_packet.tail) begin
-                            for (int j = 0; j < 32; j++) begin
-                                if (m_table[j].rob_tag == i) begin
-                                    m_table[j].t_plus  <= 0;
-                                    m_table[j].rob_tag <= 0;
-                                end
-                            end
-                        end
-                    end else begin
-                        if (i > branch_packet.rob_tag || i <= rob_map_packet.tail) begin
-                            for (int j = 0; j < 32; j++) begin
-                                if (m_table[j].rob_tag == i) begin
-                                    m_table[j].t_plus  <= 0;
-                                    m_table[j].rob_tag <= 0;
-                                end
-                            end
-                        end
+                // revert to earlier m_table state (captures incorrect overwrites)
+                m_table <= m_table_record[branch_packet.rob_tag];
+                m_table[dp_packet.dest_reg_idx].rob_tag <= rob_map_packet.rob_new_tail.rob_tag;
+                m_table[dp_packet.dest_reg_idx].t_plus <= 0;
+                // preserve t_plus (copied from above)
+                for (int i = 0; i < 32; i++) begin
+                    m_table[i].t_plus <= m_table[i].t_plus;
+                    if (cdb_packet.rob_tag !== 0) begin
+                        m_table[i].t_plus <= (m_table[i].rob_tag == cdb_packet.rob_tag && m_table[i].rob_tag != `ZERO_REG) ? 1 : m_table[i].t_plus;
                     end
                 end
+                m_table_record <= '0;
             end
         end
     end
